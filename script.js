0dc80c6c8ea73de31f755ede1168c021f072233e
Emi O <124805964+emi965@users.noreply.github.com>
2026-04-24 21:09:11 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ icon.onclick = () => {//changes from light to dark mode and vice versa
     ? "dark.png"
     : "light.png";
 };
+
+function sendMail(){
+  //parameters needed for EmailJs
+  let data= {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  console.log(data);
+  emailjs.send("service_pohx96a","template_erndast",data).then(alert("Your Email has been sent!"));//serviceID, templateID
+  
+};
+
